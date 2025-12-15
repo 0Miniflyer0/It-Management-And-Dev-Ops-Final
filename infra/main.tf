8e8e4e16b2d158b121f6b3d0316b7e82d206a81a
0Miniflyer0 <145483845+0Miniflyer0@users.noreply.github.com>
2025-12-15 15:13:03 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "flask_app" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami                    = "ami-0c02fb55956c7d316" 
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
